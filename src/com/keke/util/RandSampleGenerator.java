9019b719e60fbd70ef6e4a55b042a70661d41434
KEKE <493006007@qq.com>
2019-05-07 22:14:59 +0800

    添加注释

--- src/com/keke/util/RandSampleGenerator.java
+++ src/com/keke/util/RandSampleGenerator.java
@@ -6,6 +6,13 @@ package com.keke.util;
  */
 public class RandSampleGenerator {
 
+    /**
+     * 生成一个随机数组
+     * @param maxSize
+     * @param maxValule
+     * @param negable 表示是否可以包含负数元素
+     * @return
+     */
     public static int[] generateRandomIntegerArray(int maxSize, int maxValule, boolean negable){
         int[] arr = new int[(int)( (maxSize+1)*Math.random() )];
         for (int i = 0; i < arr.length; i++) {
@@ -17,6 +24,12 @@ public class RandSampleGenerator {
         return arr;
     }
 
+    /**
+     * 生成随机二维矩阵
+     * @param maxRowColSize
+     * @param squre 是否生成方阵
+     * @return
+     */
     public static int[][] generateRandomIntegerMatrix(int maxRowColSize, boolean squre){
         int[][] result;
         if (squre){
