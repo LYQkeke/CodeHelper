cc3fd568fb8590c6887b190f30fd5f6ae04b2d9a
KEKE <493006007@qq.com>
2019-05-08 22:24:33 +0800

    添加实体 二叉树节点

---
+++ src/com/keke/entity/TreeNode.java
@@ -0,0 +1,15 @@
+package com.keke.entity;
+
+/**
+ * Created by KEKE on 2019/5/8
+ * 二叉树节点
+ */
+public class TreeNode {
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
