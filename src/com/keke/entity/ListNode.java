c271b403cf33f94fc15c5cc482ac03eb2fcb9550
KEKE <493006007@qq.com>
2019-05-20 20:27:52 +0800

    添加 ListNode

---
+++ src/com/keke/entity/ListNode.java
@@ -0,0 +1,13 @@
+package com.keke.entity;
+
+/**
+ * Created by KEKE on 2019/5/20
+ */
+public class ListNode {
+    public int val;
+    public ListNode next = null;
+
+    public ListNode(int val){
+        this.val = val;
+    }
+}
