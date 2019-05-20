2b47f62d072253b02427960dd651e84f1e703ad2
KEKE <493006007@qq.com>
2019-05-20 21:49:01 +0800

    添加 矩形覆盖问题

---
+++ src/com/keke/notebook/jzoffer/Code_10.java
@@ -0,0 +1,19 @@
+package com.keke.notebook.jzoffer;
+
+/**
+ * Created by KEKE on 2019/5/20
+ * 矩形覆盖
+ * 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+ * 请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+ */
+public class Code_10 {
+    public int RectCover(int target) {
+        if(target==0)
+            return 0;
+        if(target==1)
+            return 1;
+        if(target==2)
+            return 2;
+        return RectCover(target-1)+RectCover(target-2);
+    }
+}
