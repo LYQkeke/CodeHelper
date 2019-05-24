8ce9a677506293dbf72eec50f3ad4e34feced83a
KEKE <493006007@qq.com>
2019-05-24 20:45:47 +0800

    set public

--- src/com/keke/entity/RandomListNode.java
+++ src/com/keke/entity/RandomListNode.java
@@ -4,11 +4,11 @@ package com.keke.entity;
  * Created by KEKE on 2019/5/24
  */
 public class RandomListNode {
-    int label;
-    RandomListNode next = null;
-    RandomListNode random = null;
+    public int label;
+    public RandomListNode next = null;
+    public RandomListNode random = null;
 
-    RandomListNode(int label) {
+    public RandomListNode(int label) {
         this.label = label;
     }
 }
