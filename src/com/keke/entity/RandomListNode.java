60ef9fb72c071c45475a5a88f2c618a9cee59f35
KEKE <493006007@qq.com>
2019-05-24 20:43:49 +0800

    添加 RandomListNode

---
+++ src/com/keke/entity/RandomListNode.java
@@ -0,0 +1,14 @@
+package com.keke.entity;
+
+/**
+ * Created by KEKE on 2019/5/24
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
