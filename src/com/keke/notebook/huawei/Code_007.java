7db4ad636bd2eab2a45d4af4a5c7c3d5c763648c
KEKE <493006007@qq.com>
2019-06-22 19:49:51 +0800

    添加 华为 取近似值

---
+++ src/com/keke/notebook/huawei/Code_007.java
@@ -0,0 +1,16 @@
+package com.keke.notebook.huawei;
+
+import java.util.Scanner;
+
+/**
+ * Created by KEKE on 2019/6/22
+ * 取近似值
+ * 写出一个程序，接受一个正浮点数值，输出该数值的近似整数值。如果小数点后数值大于等于5,向上取整；小于5，则向下取整。
+ */
+public class Code_007 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        double n = in.nextDouble();
+        System.out.println(Math.round(n));
+    }
+}
